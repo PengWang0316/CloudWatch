9546126497812b2c3665a98ee785d003f26432ae
Kevin Wang <rocwang0316@gmail.com>
2019-01-25 16:09:32 -0800

    Added some setups for the test.

--- __tests__/CloudWatch.js
+++ __tests__/CloudWatch.js
@@ -3,14 +3,15 @@ import log from '@kevinwang0316/log';
 
 import * as cloudwatch from '../src/CloudWatch';
 
+const mockPutMetricData = jest.fn().mockReturnValue({ promise: jest.fn() });
+
 jest.mock('aws-sdk', () => ({}));
 jest.mock('aws-xray-sdk', () => ({
   captureAWS: jest.fn().mockReturnValue({
-    CloudWatch: jest.fn().mockReturnValue({
-      putMetricData: jest.fn().mockReturnValue({ promise: jest.fn() }),
-    }),
+    CloudWatch: jest.fn().mockImplementation(() => ({ putMetricData: mockPutMetricData })),
   }),
 }));
+jest.mock('@kevinwang0316/log', () => ({ debug: jest.fn(), warn: jest.fn() }));
 
 describe('CloudWatch Test', () => {
   beforeAll(() => {
@@ -18,6 +19,11 @@ describe('CloudWatch Test', () => {
     process.env.AWS_LAMBDA_FUNCTION_VERSION = 'functionVersion';
     process.env.AWS_LAMBDA_LOG_GROUP_NAME = 'logGroupName';
     process.env.STAGE = 'stage';
+  });
+
+  beforeEach(() => {
+    log.debug.mockClear();
+    log.warn.mockClear();
   });
 
   test('AWSXray captureAWS is called', () => {
@@ -28,5 +34,8 @@ describe('CloudWatch Test', () => {
     const result = await cloudwatch.flush();
 
     expect(result).toBeUndefined();
+    expect(log.debug).not.toHaveBeenCalled();
+    expect(log.warn).not.toHaveBeenCalled();
+    expect(mockPutMetricData).not.toHaveBeenCalled();
   });
 });
