b1225ecdd1c9eb1d8eb3870d67d0ba172ed9bc34
Kevin Wang <rocwang0316@gmail.com>
2019-01-25 16:51:17 -0800

    Added the test case for the flush method.

--- __tests__/CloudWatch.js
+++ __tests__/CloudWatch.js
@@ -3,12 +3,12 @@ import log from '@kevinwang0316/log';
 
 import * as cloudwatch from '../src/CloudWatch';
 
-const mockPutMetricData = jest.fn().mockReturnValue({ promise: jest.fn() });
+// const mockPutMetricData = jest.fn().mockReturnValue({ promise: jest.fn() });
 
 jest.mock('aws-sdk', () => ({}));
 jest.mock('aws-xray-sdk', () => ({
   captureAWS: jest.fn().mockReturnValue({
-    CloudWatch: jest.fn().mockImplementation(() => ({ putMetricData: mockPutMetricData })),
+    CloudWatch: jest.fn().mockImplementation(() => ({ putMetricData: jest.fn().mockReturnValue({ promise: jest.fn() }) })),
   }),
 }));
 jest.mock('@kevinwang0316/log', () => ({ debug: jest.fn(), warn: jest.fn() }));
@@ -36,6 +36,32 @@ describe('CloudWatch Test', () => {
     expect(result).toBeUndefined();
     expect(log.debug).not.toHaveBeenCalled();
     expect(log.warn).not.toHaveBeenCalled();
-    expect(mockPutMetricData).not.toHaveBeenCalled();
+    // expect(mockPutMetricData).not.toHaveBeenCalled();
+  });
+
+  test('flush with no empty countDatum and timeDatum', async () => {
+    cloudwatch.incrCount('my count', 1);
+    const result = await cloudwatch.flush();
+
+    expect(result).toBeUndefined();
+    expect(log.debug).toHaveBeenCalledTimes(2);
+    expect(log.debug).toHaveBeenNthCalledWith(1, 'flushing [1] metrics to CloudWatch: my count');
+    expect(log.debug).toHaveBeenLastCalledWith('flushed [1] metrics to CloudWatch: my count');
+    expect(log.warn).not.toHaveBeenCalled();
+    // expect(mockPutMetricData).not.toHaveBeenCalled();
+  });
+
+  test('flush with no empty countDatum and timeDatum with error', async () => {
+    // AWSXray.captureAWS().CloudWatch().putMetricData.mockImplementationOnce(() => { throw new Error('error message'); });
+    log.debug.mockImplementationOnce(() => jest.fn());
+    // Use log.debug to simulate throwing error.
+    log.debug.mockImplementationOnce(() => { throw new Error('error message'); });
+    cloudwatch.incrCount('my count', 1);
+    const result = await cloudwatch.flush();
+
+    expect(result).toBeUndefined();
+    expect(log.warn).toHaveBeenCalledTimes(1);
+    expect(log.warn).toHaveBeenLastCalledWith('cloudn\'t flush [1] CloudWatch metrics', null, new Error('error message'));
+    // expect(mockPutMetricData).not.toHaveBeenCalled();
   });
 });
